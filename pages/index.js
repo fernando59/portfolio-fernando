b194a62a25c72870862c077d976a309df30e711a
Fernando <fernandomercado529@gmail.com>
2022-06-25 20:52:57 -0400

    Add darkMode

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import { useTheme } from 'next-themes';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 export default function Home() {
+  const { theme, setTheme } = useTheme()
+
+  const changeTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
   return (
     <div className={styles.container}>
       <Head>
@@ -12,21 +14,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className='dark:text-white dark:bg-black'>Hello with Tailwind</h1>
-       </main>
+        <h1 className='dark:text-white dark:bg-black'>Hello with Tailwind {theme}</h1>
+        <button onClick={changeTheme} className='bg-blue-700 dark:bg-blue-900'>Toggle Theme</button>
+      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+
     </div>
   )
 }
