271a42dbc6c5c2be7478eb174fd29861b6494158
Fernando <fernandomercado529@gmail.com>
2022-07-01 13:48:40 -0400

    add togle switch

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,11 @@
+import { ToggleThemeButton } from "./Header/ToggleThemeButton"
 
 export const Header = () => {
     return <>
         <header>
 
             <div>Header</div>
+            <ToggleThemeButton/>
         </header>
     </>
 }
